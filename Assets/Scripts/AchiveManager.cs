d6aeccda38ebb8299b514ef4445ebed8c1b47688
ㅇㅅㅇ <jhy041020@handong.ac.kr>
2025-07-12 04:12:56 +0900

    [Feat] 업적 달성 로직 구현

--- Assets/Scripts/AchiveManager.cs
+++ Assets/Scripts/AchiveManager.cs
@@ -43,8 +43,30 @@ public class AchiveManager : MonoBehaviour
         }
     }
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        foreach (Achive achive in achives)
+        {
+            CheckAchive(achive);
+        }
+    }
+
+    void CheckAchive(Achive achive)
+    {
+        bool isAchive = false;
+
+        switch (achive)
+        {
+            case Achive.UnlockPotato:
+                isAchive = GameManager.instance.kill >= 10;
+                break;
+            case Achive.UnlockBean:
+                isAchive = GameManager.instance.gameTime == GameManager.instance.maxGameTime;
+                break;
+        }
+        if (isAchive && PlayerPrefs.GetInt(achive.ToString()) == 0)
+        {
+            PlayerPrefs.SetInt(achive.ToString(), 1);
+        }
     }
 }
