85b92197c52f2cc2d28bb6e5999a39a01142be90
ㅇㅅㅇ <jhy041020@handong.ac.kr>
2025-07-14 16:34:19 +0900

    [Feat] 로직 보완하기 1
    무한맵 재배치 보안

--- Assets/Scripts/Reposition.cs
+++ Assets/Scripts/Reposition.cs
@@ -18,15 +18,23 @@ public class Reposition : MonoBehaviour
 
         Vector3 playerPos = GameManager.instance.player.transform.position;
         Vector3 myPos = transform.position;
-        float diffX = Mathf.Abs(playerPos.x - myPos.x); // 절댓값
-        float diffY = Mathf.Abs(playerPos.y - myPos.y);
-        Vector3 playerDir = GameManager.instance.player.inputVec;
-        float dirX = playerDir.x < 0 ? -1 : 1;
-        float dirY = playerDir.y < 0 ? -1 : 1;
+        // float diffX = Mathf.Abs(playerPos.x - myPos.x); // 절댓값
+        // float diffY = Mathf.Abs(playerPos.y - myPos.y);
+        // Vector3 playerDir = GameManager.instance.player.inputVec;
+        // float dirX = playerDir.x < 0 ? -1 : 1;
+        // float dirY = playerDir.y < 0 ? -1 : 1;
 
         switch (transform.tag)
         {
             case "Ground":
+                float diffX = playerPos.x - myPos.x; // 절댓값
+                float diffY = playerPos.y - myPos.y;
+                float dirX = diffX < 0 ? -1 : 1;
+                float dirY = diffY < 0 ? -1 : 1;
+
+                diffX = Mathf.Abs(diffX);
+                diffY = Mathf.Abs(diffY);
+
                 if (diffX > diffY)
                 {
                     transform.Translate(Vector3.right * dirX * 80);
@@ -39,7 +47,9 @@ public class Reposition : MonoBehaviour
             case "Enemy":
                 if (coll.enabled)
                 {
-                    transform.Translate(playerDir * 40 + new Vector3(Random.Range(3f, -3f), Random.Range(3f, -3f), 0));
+                    Vector3 dist = playerPos - myPos;
+                    Vector3 rand = new Vector3(Random.Range(-3, 3), Random.Range(-3, 3), 0);
+                    transform.Translate(rand + dist * 2);
                 }
                 break;
         }
